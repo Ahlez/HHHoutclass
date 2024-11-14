bc276dfe8b326a4c394c94d853a28ca86681d16a
Ahlez <harezcoding@gmail.com>
2024-11-14 11:49:25 +0800

    Update Sourcecode.py

--- Sourcecode.py
+++ Sourcecode.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 st.header("Sourcecode")
-st.write(pd.DataFrame({}))
+st.write(pd.DataFrame({
     'Input': ['TV', 'Radio', 'Newspaper'],
     'Output': [Sales]
+})
