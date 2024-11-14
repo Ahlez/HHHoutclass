d71437cfd867755ae58c0a7680a9102ba1cce794
Ahlez <harezcoding@gmail.com>
2024-11-14 14:18:27 +0800

    Delete Sourcecode.py

--- Sourcecode.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-st.header("Sourcecode")
-st.write(pd.DataFrame({
-    'Input': ['TV', 'Radio', 'Newspaper'],
-    'Output': [Sales]
-})
