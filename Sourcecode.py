5264c35ff7bbda523b672e71aa30aab5cae2f65a
Ahlez <harezcoding@gmail.com>
2024-11-14 11:46:12 +0800

    Update Sourcecode.py

--- Sourcecode.py
+++ Sourcecode.py
@@ -3,6 +3,6 @@ import numpy as np
 import pandas as pd
 
 st.header("Sourcecode")
-st.write(pd.DataFrame({)
+st.write(pd.DataFrame({})
     'Students': ['John', 'Lofa', 'Siti', 'Amy'],
     'Attendance Status': ['yes', 'yes', 'yes', 'no']
