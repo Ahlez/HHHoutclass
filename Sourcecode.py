2117be7a5cbfd25ae0495041a08ce6f51a158328
Ahlez <harezcoding@gmail.com>
2024-11-14 11:40:38 +0800

    Add files via upload

---
+++ Sourcecode.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("Sourcecode")
+st.write(pd.DataFrame({
+    'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+    'Attendance Status': ['yes', 'yes', 'yes', 'no']
